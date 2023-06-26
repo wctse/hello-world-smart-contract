0d7e42e0d07e3fd8616a2ba2a08166250f8ad934
wctse <47619083+wctse@users.noreply.github.com>
2023-06-26 23:04:15 +0800

    initialize repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction
+Simple hello world smart contract for testing custom scripts interacting with EVM blockchains.
+
+# Author
+Chak Tse
---
+++ contracts/contract.sol
@@ -0,0 +1,42 @@
+// Specifies the version of Solidity, using semantic versioning.
+// Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
+pragma solidity >=0.8.19;
+
+// Defines a contract named `HelloWorld`.
+// A contract is a collection of functions and data (its state). Once deployed, a contract resides at a specific address on the Ethereum blockchain. Learn more: https://solidity.readthedocs.io/en/v0.5.10/structure-of-a-contract.html
+contract HelloWorld {
+   
+   event UpdatedMessages(string oldStr, string newStr);
+   event SayHelloWorld(string helloWorld);
+
+   string public message;
+   mapping(address => bool) public addressInteracted;
+
+   // Similar to many class-based object-oriented languages, a constructor is a special function that is only executed upon contract creation.
+   // Constructors are used to initialize the contract's data. Learn more:https://solidity.readthedocs.io/en/v0.5.10/contracts.html#constructors
+   constructor(string memory initMessage) {
+
+      // Accepts a string argument `initMessage` and sets the value into the contract's `message` storage variable).
+      message = initMessage;
+   }
+
+   // A public function that accepts a string argument and updates the `message` storage variable.
+   function update(string memory newMessage) public {
+      string memory oldMsg = message;
+      message = newMessage;
+      emit UpdatedMessages(oldMsg, newMessage);
+
+      if (!addressInteracted[msg.sender]) {
+         addressInteracted[msg.sender] = true;
+      }
+   }
+
+   function sayHelloWorld() public {
+      emit SayHelloWorld("Hello World");
+
+      if(!addressInteracted[msg.sender]) {
+         addressInteracted[msg.sender] = true;
+      }
+   }
+
+}
